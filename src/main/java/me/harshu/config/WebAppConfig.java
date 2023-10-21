b4627b6bdafe9e78ffcbb183357825a8c9ff96f9
Harshana <mailtoharshanaonlive@gmail.com>
2023-10-21 07:31:50 +0530

    Initial commit

---
+++ src/main/java/me/harshu/config/WebAppConfig.java
@@ -0,0 +1,11 @@
+package me.harshu.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "me.harshu")
+public class WebAppConfig {
+}
